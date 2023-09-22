d075cac19d7992ed812b8cd17caf5b7d179de0ef
Wiatrace <135695007+Wiatrace@users.noreply.github.com>
2023-09-22 19:26:23 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    println("Количество подряд идущих одинаковых символов во введенной строке: " + first(readln()))
+
+}
+fun first(string: String): String {
+    var a = string[0]
+    var result = ""
+    var count = 1
+    var i = 1
+    for (item in string.substring(1)) {
+        i++
+        if (a != item || i == string.length) {
+            if (count > 1) {
+                result += "$a$count"
+            } else {
+                result += a
+            }
+            count = 1
+            a = item
+        } else {
+            count++
+        }
+    }
+    return result
+}
